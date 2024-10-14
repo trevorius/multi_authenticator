e0957c5eff35635d404cf0da1cce69f998928ca3
trevo <trevorius45@gmail.com>
2024-10-14 09:19:32 -0400

    pretyfy

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,10 +1,14 @@
-import SignInForm from '@/components/login/SignInForm'
-import SignupResult from '@/components/login/SignupResult'
-export default function SignInPage({searchParams}: {searchParams: {message: string}}) {
+import SignInForm from '@/components/login/SignInForm';
+import SignupResult from '@/components/login/SignupResult';
+export default function SignInPage({
+  searchParams,
+}: {
+  searchParams: { message: string };
+}) {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-          <SignupResult message={searchParams.message} />
+    <div className='flex min-h-screen items-center justify-center bg-gray-100'>
+      <SignupResult message={searchParams.message} />
       <SignInForm />
     </div>
-  )
+  );
 }
--- src/components/login/SignupResult.tsx
+++ src/components/login/SignupResult.tsx
@@ -1,32 +1,39 @@
-'use client'
+'use client';
 
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 
 interface SignupResultProps {
-  error?: string | null
-  success?: boolean
-  message?: string|null
+  error?: string | null;
+  success?: boolean;
+  message?: string | null;
 }
 
-export default function SignupResult({ error, success, message }: SignupResultProps) {
-  const router = useRouter()
+export default function SignupResult({
+  error,
+  success,
+  message,
+}: SignupResultProps) {
+  const router = useRouter();
 
   useEffect(() => {
     if (success) {
       const timer = setTimeout(() => {
-        router.push('/login')
-      }, 2000)
-      return () => clearTimeout(timer)
+        router.push('/login');
+      }, 2000);
+      return () => clearTimeout(timer);
     }
-  }, [success, router])
+  }, [success, router]);
 
- 
-    return (
-      <Alert>
-        <AlertTitle>{message.includes("Failed") ? "Error" : "Success"}</AlertTitle>
-        <AlertDescription>{message || "Sign up successful! Redirecting to login..."}</AlertDescription>
-      </Alert>
-    )
-  }
+  return (
+    <Alert>
+      <AlertTitle>
+        {message.includes('Failed') ? 'Error' : 'Success'}
+      </AlertTitle>
+      <AlertDescription>
+        {message || 'Sign up successful! Redirecting to login...'}
+      </AlertDescription>
+    </Alert>
+  );
+}
