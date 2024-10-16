7789d130ee7506d0935b07d3b8405ee8f92eb6e8
trevo <trevorius45@gmail.com>
2024-10-15 22:19:05 -0400

    use server on actions

--- src/components/EnvironmentCard.tsx
+++ src/components/EnvironmentCard.tsx
@@ -1,15 +1,16 @@
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { Environment, Code2Fa } from "@prisma/client";
-import { Code2FaTable } from "@/components/Code2FaTable";
-import { DeleteButton } from "@/components/DeleteButton";
-import { deleteEnvironment } from "@/app/protected/dashboard/environments/actions";
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { Environment, Code2Fa } from '@prisma/client';
+import { Code2FaTable } from '@/components/Code2FaTable';
+import { DeleteButton } from '@/components/DeleteButton';
+import { deleteEnvironment } from '@/app/protected/dashboard/environments/actions';
 
 // Adjust the deleteEnvironment function to accept string | number
 const adjustedDeleteEnvironment = async (id: string | number) => {
+  'use server';
   const result = await deleteEnvironment(id.toString());
   return {
     ...result,
-    success: result.success === "true", // Convert success to boolean
+    success: result.success === 'true', // Convert success to boolean
   };
 };
 
@@ -21,12 +22,12 @@ export function EnvironmentCard({
   return (
     <Card>
       <CardHeader>
-        <CardTitle className="text-xl font-bold flex flex-wrap justify-between">
-          <div className="flex flex-col">
+        <CardTitle className='text-xl font-bold flex flex-wrap justify-between'>
+          <div className='flex flex-col'>
             <div>{environment.name}</div>
             <div>{environment.type}</div>
           </div>
-          <div className="">
+          <div className=''>
             <DeleteButton
               deleteAction={adjustedDeleteEnvironment}
               id={environment.id}
